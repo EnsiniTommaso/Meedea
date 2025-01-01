c296058b68b1c6d1b982185915a6fd4747a69cde
Tommy <tommaso.ensini@gmail.com>
2025-01-01 15:30:21 +0100

    .env works

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,11 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import router from "./services/gateway.js"
+import 'dotenv/config'
 
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
+
 
 app.use(express.json())
 app.use(cookieParser())
